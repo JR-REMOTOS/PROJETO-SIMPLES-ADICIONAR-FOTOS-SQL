65d55aea137bf542e4f929b23e8ce4712264e4f1
JR-REMOTOS <122941429+JR-REMOTOS@users.noreply.github.com>
2023-06-16 20:08:04 -0300

    Add files via upload

---
+++ projetoAdicionarFotosSQL/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace projetoAdicionarFotosSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonBuscar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonSelecionarImagem = new System.Windows.Forms.Button();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonBuscar
+            // 
+            this.buttonBuscar.Location = new System.Drawing.Point(267, 44);
+            this.buttonBuscar.Name = "buttonBuscar";
+            this.buttonBuscar.Size = new System.Drawing.Size(114, 23);
+            this.buttonBuscar.TabIndex = 0;
+            this.buttonBuscar.Text = "Buscar";
+            this.buttonBuscar.UseVisualStyleBackColor = true;
+            this.buttonBuscar.Click += new System.EventHandler(this.buttonBuscar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 99);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(386, 247);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // buttonSelecionarImagem
+            // 
+            this.buttonSelecionarImagem.Location = new System.Drawing.Point(12, 352);
+            this.buttonSelecionarImagem.Name = "buttonSelecionarImagem";
+            this.buttonSelecionarImagem.Size = new System.Drawing.Size(114, 23);
+            this.buttonSelecionarImagem.TabIndex = 3;
+            this.buttonSelecionarImagem.Text = "Selecionar Imagem";
+            this.buttonSelecionarImagem.UseVisualStyleBackColor = true;
+            this.buttonSelecionarImagem.Click += new System.EventHandler(this.buttonSelecionarImagem_Click);
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Location = new System.Drawing.Point(151, 378);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(121, 43);
+            this.buttonSalvar.TabIndex = 4;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nome :";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 433);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSalvar);
+            this.Controls.Add(this.buttonSelecionarImagem);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonBuscar);
+            this.Name = "Form1";
+            this.Text = "AdiconarImagensSQL";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBuscar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonSelecionarImagem;
+        private System.Windows.Forms.Button buttonSalvar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ projetoAdicionarFotosSQL/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projetoAdicionarFotosSQL
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        //CONEXÃO BBANCO
+        const string string_conexao = @"AQUI ADICIONE SE BANCO LOCAL OU ONLINE";
+        //ABRIR CONEXÃO
+        SqlConnection conexao = new SqlConnection(string_conexao);
+
+
+        public object Imageformat { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //BOTAO SELECIONAR IMAGEM DENTRO DO PC PARA SALVAR
+        private void buttonSelecionarImagem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string nome = openFileDialog1.FileName;
+                bmp = new Bitmap(nome);
+                pictureBox1.Image = bmp;
+            }
+        }
+        //BOTAO PARA SALVAR A IMAGEM NO BANCO
+        private void buttonSalvar_Click(object sender, EventArgs e)
+        {
+            MemoryStream memory = new MemoryStream();
+
+            bmp.Save(memory, ImageFormat.Bmp);
+
+            byte[] foto = memory.ToArray();
+
+            const string string_conexao = @"AQUI ADICIONE SE BANCO LOCAL OU ONLINE";
+
+            SqlConnection conexao = new SqlConnection(string_conexao);
+            // SUA TABELA E EM NOME, IMAGEM A COLUNA DA ASUA TABELA
+            SqlCommand comando = new SqlCommand("INSERT INTO SUA TABELA (nome,imagem) VALUES (@nome, @imagem)", conexao);
+
+            SqlParameter nome = new SqlParameter("@nome", SqlDbType.VarChar);
+            SqlParameter imagem = new SqlParameter("@imagem", SqlDbType.Binary);
+            nome.Value = textBox1.Text;
+            imagem.Value = foto;
+
+            comando.Parameters.Add(nome);
+            comando.Parameters.Add(imagem);
+
+            try
+            {
+                conexao.Open();
+
+                comando.ExecuteNonQuery();
+
+                MessageBox.Show("Imagem Gravada Com Sucesso !");
+            }
+
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+        //BOTAO PARA  BUSCAR A IMAGEM NO BANCO E APARECER NO pictureBox
+        private void buttonBuscar_Click(object sender, EventArgs e)
+        {
+            SqlCommand comando = new SqlCommand("SELECT * FROM SUA TABELA WHERE nome = @nome", conexao);
+            SqlParameter nome = new SqlParameter("@nome", SqlDbType.VarChar);
+            nome.Value = textBox1.Text;
+            comando.Parameters.Add(nome);
+
+            //try
+            {
+                conexao.Open();
+
+                SqlDataReader reader = comando.ExecuteReader();
+
+                reader.Read();
+
+                if (reader.HasRows)
+                {
+                    textBox1.Text = reader[1].ToString();
+
+                    byte[] Foto = (byte[])(reader[2]);
+                    if (Foto == null)
+                        pictureBox1.Image = null;
+                    else
+                    {
+                        MemoryStream memory = new MemoryStream(Foto);
+
+                        pictureBox1.Image = Image.FromStream(memory);
+                    }
+
+                }
+
+                conexao.Close();
+
+            }
+            //catch (Exception E)
+            {
+            //    MessageBox.Show(E.Message);
+            }
+        }
+    }
+}
